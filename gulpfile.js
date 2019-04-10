b2a5362f0718eef594677c6b985d427e50cffaff
Nick Osborn <nosborn@users.noreply.github.com>
2019-04-10 09:56:48 +0800

    Migrate to Netlify

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,7 @@
 
 const gulp = require('gulp');
 
-const awspublish = require('gulp-awspublish');
-const cloudfront = require('gulp-cloudfront');
 const pug = require('gulp-pug');
-const revAll = require('gulp-rev-all');
 const sitemap = require('gulp-sitemap');
 
 // BUILD TASKS
@@ -31,37 +28,6 @@ gulp.task('sitemap', () => {
     siteUrl: 'https://osborn.io'
   }))
   .pipe(gulp.dest('build/'));
-});
-
-// DEPLOY TASKS
-
-gulp.task('deploy', () => {
-  const publisher = awspublish.create({
-    region: process.env.AWS_DEFAULT_REGION,
-    params: {
-      Bucket: process.env.CONTENT_BUCKET
-    }
-  });
-
-  const headers = {
-    // 'Cache-Control': 'max-age=315360000, no-transform, public',
-    'x-amz-acl': 'private'
-  };
-
-  gulp.src('build/**')
-    .pipe(revAll.revision({
-      dontGlobal: [
-        'ping.txt',
-        'robots.txt',
-        'sitemap.xml'
-      ]
-    }))
-    .pipe(publisher.publish(headers))
-    .pipe(awspublish.reporter())
-    .pipe(cloudfront({
-      bucket: process.env.CONTENT_BUCKET,
-      distributionId: process.env.CLOUDFRONT_DISTRIBUTION_ID
-    }));
 });
 
 // LOCAL TASKS
