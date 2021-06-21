a066e62bf1b9ce4c82d06c63d9779bba757d21d6
Nick Osborn <nosborn@users.noreply.github.com>
2021-06-21 08:01:46 +0800

    Configure eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,42 @@
 (() => {
-  "use strict";
+  'use strict'
 
-  const gulp = require("gulp"),
-    awspublish = require("gulp-awspublish"),
-    pug = require("gulp-pug");
+  const gulp = require('gulp')
+  const awspublish = require('gulp-awspublish')
+  const pug = require('gulp-pug')
 
-  function html() {
+  function html () {
     return gulp
-      .src("src/*.pug")
+      .src('src/*.pug')
       .pipe(pug())
-      .pipe(gulp.dest("dist/"));
+      .pipe(gulp.dest('dist/'))
   }
-  exports.html = html;
+  exports.html = html
 
-  function plain() {
-    return gulp.src("src/**/!(*.pug)", { dot: true }).pipe(gulp.dest("dist/"));
+  function plain () {
+    return gulp.src('src/**/!(*.pug)', { dot: true }).pipe(gulp.dest('dist/'))
   }
-  exports.plain = plain;
+  exports.plain = plain
 
-  function publish() {
-    var headers = {
-      "x-amz-acl": "private"
-    };
-    var publisher = awspublish.create({
+  function publish () {
+    const headers = {
+      'x-amz-acl': 'private'
+    }
+    const publisher = awspublish.create({
       params: {
-        Bucket: "osborn.io"
+        Bucket: 'osborn.io'
       },
-      region: "ap-southeast-1"
-    });
+      region: 'ap-southeast-1'
+    })
     return gulp
-      .src("dist/**/*", { dot: true })
+      .src('dist/**/*', { dot: true })
       .pipe(publisher.publish(headers))
       .pipe(publisher.sync())
-      .pipe(awspublish.reporter());
+      .pipe(awspublish.reporter())
   }
-  exports.publish = publish;
+  exports.publish = publish
 
-  exports.build = gulp.series(html, plain);
-  exports.default = exports.build;
-  exports.deploy = gulp.series(publish);
-})();
+  exports.build = gulp.series(html, plain)
+  exports.default = exports.build
+  exports.deploy = gulp.series(publish)
+})()
